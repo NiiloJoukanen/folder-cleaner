3920e1fbc34b92f4b109a5f6e8b2078f6dfa6db3
Niilo Joukanen <niilo.joukanen@gmail.com>
2020-09-24 08:05:32 +0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -4,8 +4,8 @@ from shutil import move
 if __name__ == "__main__":
     root_path = Path.home() / "Downloads" #use ... / "Desktop" to clear your desktop
 
-    sort_files_extensions = {".txt": "my_text_files", ".py": "my_python_files", ".exe": "my_executable_files", ".png": "my_pictures", ".jpeg": "my_pictures"} #please customise these to your own preferences
-    other_files = "other_files" #all other files will be stored here
+    sort_files_extensions = {".txt": "my_text_files", ".py": "my_python_files", ".exe": "my_executable_files", ".png": "my_pictures", ".jpeg": "my_pictures"}  # please customise these to your own preferences
+    other_files = "other_files"  # all other files will be stored here
 
     for item in list(sort_files_extensions.keys()):
         if not (root_path / sort_files_extensions[item]).exists():
@@ -18,17 +18,17 @@ if __name__ == "__main__":
 
     files_moved_total = 0
 
-    for item in root_path.glob("*.*"): #find all files in the root directory
-        for item_2 in list(sort_files_extensions.keys()): #get all file extensions
-            if str(item).endswith(item_2): #check if file endswith specific file extension
+    for item in root_path.glob("*.*"):  # find all files in the root directory
+        for item_2 in list(sort_files_extensions.keys()):  # get all file extensions
+            if str(item).endswith(item_2):  # check if file endswith specific file extension
                 move(str(root_path / item), str(root_path / sort_files_extensions[item_2])) #move item to the corresponding folder
                 print(f"Moved item: {item}")
-                files_moved_total += 1 #count all files moved
+                files_moved_total += 1  # count all files moved
 
-    for item in root_path.glob("*.*"): #find all files left in the root directory
-        if not item.is_dir(): #check if file is not a directory (folder)
-            move(str(root_path / item), str(root_path / other_files)) #move file to other_files folder
+    for item in root_path.glob("*.*"):  # find all files left in the root directory
+        if not item.is_dir():  # check if file is not a directory (folder)
+            move(str(root_path / item), str(root_path / other_files))  # move file to other_files folder
             print(f"Moved item: {item}")
-            files_moved_total += 1 #count all files moved
+            files_moved_total += 1  # count all files moved
 
     print(f"\nCleared your folder! {files_moved_total} files were moved!")
